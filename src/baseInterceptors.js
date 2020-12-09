88982dd0dbcb4b37d9d1294a75fd9ee7be41860b
ccwq <scwqd@qq.com>
2020-12-09 20:32:52 +0800

    backup

--- src/baseInterceptors.js
+++ src/baseInterceptors.js
@@ -1,33 +1,13 @@
 import get from "lodash/get";
 import set from "lodash/set";
 
-//收集常用的拦截器
-export const pathFixer = {
-    before(config){
-
-        const {url} = config;
-
-        if (typeof location != "undefined") {
-
-            const {origin: pageOrigin, pathname: pagePathName} = location;
-
-            //相对于当前html
-            if (url.startsWith("./")) {
-                const dirPath = pagePathName.split("/").slice(0, -1).join("/");
-                let dir = pageOrigin + "/" + (dirPath || "");
-                config.url = dir + "/" + url.substr(2);
-            }
-
-            //相对于跟
-            if (url.startsWith("/")) {
-                config.url = pageOrigin + url;
-            }
-        }
-    }
-}
 
 
 export default [
+
+    /**
+     * 处理/和./开头的请求
+     */
     {
         key:"*",
         name:"pathFixer",
@@ -56,18 +36,15 @@ export default [
                 }
             }
         }
-    }
-]
+    },
 
 
-const contentTypeField = "headers.Content-Type";
+    /**
+     * 设置content-type的快捷属性
+     * ctype="json"
+     * ctype="form"
+     */
 
-/**
- * 设置content-type的快捷属性
- * ctype="json"
- * ctype="form"
- */
-export default [
     {
         key:"*",
         name:"content-type-show-cut",
@@ -85,3 +62,7 @@ export default [
         }
     }
 ]
+
+
+const contentTypeField = "headers.Content-Type";
+
